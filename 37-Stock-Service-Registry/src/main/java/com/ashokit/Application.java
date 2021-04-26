480297f58d3bf5d4885fae99b6df76f06f4e1cc4
vyavhare6394 <ankush.vyavhare63@gmail.com>
2021-04-26 16:02:22 +0530

    microservices-ashokit

--- 37-Stock-Service-Registry/src/main/java/com/ashokit/Application.java
+++ 37-Stock-Service-Registry/src/main/java/com/ashokit/Application.java
@@ -3,10 +3,8 @@ package com.ashokit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
 @EnableEurekaServer
 public class Application {
 
